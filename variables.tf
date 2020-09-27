8901d4ead0133a3f60002933b9b56a87f8e18756
Pranab Paul <pranabp@microsoft.com>
2020-09-28 00:38:17 +0100

    first commit

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "loc" {
+    description = "Default Azure Region"
+    default = "West Europe"
+}
+
+variable tags {
+    default = {
+        source = "citadel"
+        env = "training"
+    }
+}
